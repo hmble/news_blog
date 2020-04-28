f4663c8b600457e8dbf82d52f7727a046a097430
Samyak Bakliwal <w3bcode@gmail.com>
2020-04-28 13:13:57 +0530

    Add file data before extracted link

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,15 @@ pub fn append_to_file(
 
     let mut start: usize = 0;
     let mut end: usize = 0;
+    let mut links: Vec<String> = Vec::new();
+    let mut file_data = String::new();
     for (i, line) in content.lines().enumerate() {
+        if line.starts_with("Number: ") {
+            file_data = format!("{} ", line);
+        }
+        if line.starts_with("Date: ") {
+            file_data = format!("{} {} \n\n", file_data, line);
+        }
         if line.starts_with("## News") {
             start = i;
         }
@@ -37,21 +45,22 @@ pub fn append_to_file(
         }
     }
 
-    let mut links: Vec<&str> = Vec::new();
+    links.push(file_data.to_string());
     for (i, line) in content.lines().enumerate() {
         if i > start && i < end {
             if line.is_empty() {
                 continue;
             }
-            links.push(line);
+            links.push(format!("{}\n", line));
         }
         if i == end {
             break;
         }
     }
+    links.push("\n".to_string());
 
     for line in &links {
-        write!(file, "{}\n", line)?;
+        write!(file, "{}", line)?;
     }
 
     Ok(())
